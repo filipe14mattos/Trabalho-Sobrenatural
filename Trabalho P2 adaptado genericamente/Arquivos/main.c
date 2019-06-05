c60552f84eb9b6f645e0169149b9adc31ca6b27d
Filipe Magioni Mattos <49210665+filipe14mattos@users.noreply.github.com>
2019-06-04 21:51:18 -0300

    Add files via upload

--- Trabalho P2 adaptado genericamente/Arquivos/main.c
+++ Trabalho P2 adaptado genericamente/Arquivos/main.c
@@ -4,6 +4,12 @@
 #include <math.h>
 #define TAM 50
 
+typedef struct Mundo{
+	int numhabdem;
+	int numhabhum;	
+	int numhabanj;
+}Mundo;
+
 typedef struct Demonios{
   char nome[TAM];
   float escuridao, visao;
@@ -19,9 +25,10 @@ typedef struct Humanos{
   float forca;
 }Humanos;
 
-void leranjos (Anjos *Anjos){
+void leranjos (Anjos *Anjos, Mundo *m){
 	FILE* arq;
 	arq = fopen("anjos.txt", "r");
+	m->numhabanj = 0;
 	struct Anjos anjo;
 	char nome_cidade[50];
 	int i, j, n=0, num_cidades = 0, num_habitantes = 0;
@@ -29,166 +36,149 @@ void leranjos (Anjos *Anjos){
 	fscanf(arq, "%i", &num_cidades);
 	printf("ANJOS\n");
 	for(i=0; i<num_cidades; i++){
-		fscanf(arq,"\t%s\t%i",nome_cidade, &num_habitantes);
-		//como no arquivo existe um tab entre as informações uso o \t psra fazer essa leitura
+		fscanf(arq,"%s %i",nome_cidade, &num_habitantes);
 		for(j=0; j<num_habitantes; j++){
 			fscanf(arq,"%s %f %f %f", anjo.nome,&anjo.capfly,&anjo.peso,&anjo.altura);
-			//printf("%s %f %f %f", Anjos.nome, Anjos.capfly, Anjos.peso, Anjos.altura);
 			calcforcacidades = calcforcacidades + (anjo.capfly*(anjo.altura * anjo.altura)*anjo.peso);
 			Anjos[n] = anjo;
 			n++;
 		}
+		m->numhabanj+=num_habitantes;
 		printf("\nA cidade %s tem um total de foca de: %.2f",nome_cidade, calcforcacidades );
 		calcforcacidades = 0.0;
     }
 	fclose(arq);
-    //colocar essa mesma logica nas demais funcoes
 }
-
 
 void lerdemonios (Demonios *Demonios){
 	FILE* arq;
 	arq = fopen("demonios.txt", "r");
 	int i,j, num_cidades = 0, num_habitantes = 0, n = 0;
 	char nome_cidade[50];
-	float calcforcacidades  = 0.0, dens = 0.0;
+	float calcforcacidades  = 0.0, subforca = 0.0;
 	struct Demonios demonio;
 	
 	fscanf(arq, "%i", &num_cidades);
-	//fscanf(arq,"\t%s\t%i",nome_cidade, &num_habitantes);
-	//fscanf(arq, "%s %i", Demonios[0].cidade, &Demonios[0].numhab);
 	printf("\n\nDEMONIOS\n");
 	for(i=0; i<num_cidades; i++){
-		fscanf(arq,"\t%s\t%i",nome_cidade, &num_habitantes);
-		
+		fscanf(arq,"%s %i",nome_cidade, &num_habitantes);
 		for(j=0; j<num_habitantes; j++){
 			fscanf(arq,"%s %f %f", demonio.nome,
-		                       &demonio.escuridao,
-                               &demonio.visao);
-
-			dens = (demonio.escuridao - (demonio.visao/2));				   
-			//calcforcacidades = calcforcacidades + (pow(demonio.escuridao - (demonio.visao/2)), 2);	
-			calcforcacidades = calcforcacidades + (pow(dens,2));
+		                       	   &demonio.escuridao,
+                               	   &demonio.visao);
+			subforca = (demonio.escuridao - (demonio.visao/2));				   	
+			calcforcacidades = calcforcacidades + (pow(subforca,2));
 			Demonios[n] = demonio;
 			n++;				   
 		}
 		printf("\nA cidade %s tem um total de foca de: %.2f",nome_cidade, calcforcacidades );
 		calcforcacidades = 0.0;
-	
 	}
 	printf("\n\n");
 	fclose(arq);
-}//Colocar aqui dentro para ler os arquivos
+}
 
-void lerhumanos (Humanos *humanos){
+void lerhumanos (Humanos *Humanos, Mundo *m){
 	FILE* arq;
 	arq = fopen("humanos.txt", "r");
-	int i, num_cidades = 0, num_habitantes = 0;
+	m->numhabhum = 0;
+	int i,j, num_cidades = 0, num_habitantes = 0;
 	char nome_cidade[50];
 	float calcforcacidades = 0.0;
-	
 	fscanf(arq, "%i", &num_cidades);
-	//fscanf(arq, "%s %i", Humanos[0].cidade, &Humanos[0].numhab);
-	fscanf(arq,"\t%s\t%i",nome_cidade, &num_habitantes);
 	printf("\n\nHUMANOS\n");
-	for(i=0; i<num_habitantes; i++){
-		fscanf(arq,"%s %f", humanos[i].nome,
-		                    &humanos[i].forca);
+	for(i=0; i<num_cidades;i++){
+	fscanf(arq,"%s %i",nome_cidade, &num_habitantes);
+		for(j=0; j<num_habitantes; j++){
+			fscanf(arq,"%s %f", Humanos[j].nome,
+		                        &Humanos[j].forca);
 	
-		calcforcacidades = calcforcacidades + (humanos[i].forca);
-	}
+		calcforcacidades = calcforcacidades + (Humanos[j].forca);
+		}
+	m->numhabhum+=num_habitantes;	
 	printf("\nA cidade %s tem um total de foca de: %.2f",nome_cidade, calcforcacidades);
-	//calcforcacidades = 0.0;
-	
+	calcforcacidades = 0.0;
+	}
 	fclose(arq);
-}//Colocar aqui dentro para ler os arquivos
-
-/*
-void printaranjos (Anjos *anjos){
-	int i, j, k;
-    printf("\n%s %d\n", anjos[0].cidade,
-                        anjos[0].numhab);
-	for(i=1; i<=anjos[0].numhab;i++){
-		printf("%s %f %f %f\n", anjos[i].nome,
-                            anjos[i].capfly,
-                            anjos[i].peso,
-                            anjos[i].altura);
-	}
-	printf("\n%s %d\n", anjos[i].cidade,
-                        anjos[i].numhab);
-    j=i;
-    i++;
-    k=anjos[j].numhab;
-    for(k==anjos[j].numhab; k<=anjos[j].numhab; i++){
-		printf("%s %f %f %f\n", anjos[i].nome,
-                                anjos[i].capfly,
-                                anjos[i].peso,
-                                anjos[i].altura);
-        k++;
-	}
 }
-*/
-/*
-void printardemonios (Demonios *demonios){
-	int i,k,j;
-    printf("\n%s %d\n", demonios[0].cidade,
-                        demonios[0].numhab);
-	for(i=1; i<=demonios[0].numhab;i++){
-	printf("\n%s %f %f\n", demonios[i].nome,
-                           demonios[i].escuridao,
-                           demonios[i].visao);
+
+void somaAanjo(Anjos *Anjos, Mundo m, float *c){
+	int i = 0, j;
+	int quantA = 0;
+	int tam = strlen(Anjos[0].nome);
+	for(i = 0;i<m.numhabanj;i++){
+		for(j = 0;j<tam;j++){
+			if(Anjos[i].nome[j] == 'a'){
+			quantA++;
+			}
+		}
+		if(quantA == 2){
+		*c+= (Anjos[i].capfly*(Anjos[i].altura * Anjos[i].altura)*Anjos[i].peso);
+		}
+		quantA = 0;
 	}
-	printf("\n%s %d\n", demonios[i].cidade,
-                        demonios[i].numhab);
-    j=i;
-    i++;
-    k=demonios[j].numhab;
-    for(k==demonios[j].numhab; k<=demonios[j].numhab; i++){
-		printf("\n%s %f %f\n", demonios[i].nome,
-                           demonios[i].escuridao,
-                           demonios[i].visao);
-        k++;
+} 
+
+void somaAhumano(Humanos *Humanos, Mundo m, float * c){
+	int i = 0, j;
+	int quantA = 0;
+	int tam = strlen(Humanos[0].nome);
+	float calcforcacidades = 0.0;
+	for(i = 0;i<m.numhabhum;i++){
+		for(j = 0;j<tam;j++){
+			if(Humanos[i].nome[j] == 'a'){
+			quantA++;
+			}
+		}
+		if(quantA == 2){
+		*c += Humanos[i].forca; 
+		}
+		quantA = 0;
 	}
-	printf("\n%s %d\n", demonios[i].cidade,
-                        demonios[i].numhab);
-    j=i;
-    i++;
-    k=demonios[j].numhab;
-    for(k==demonios[j].numhab; k<=demonios[j].numhab; i++){
-		printf("\n%s %f %f\n", demonios[i].nome,
-                           demonios[i].escuridao,
-                           demonios[i].visao);
-        k++;
+} 
+
+void somaAdemonios(Demonios *Demonios, Mundo m, float *c){
+	int i = 0, j;
+	int quantA = 0;
+	int tam = strlen(Demonios[0].nome);
+	float calcforcacidades = 0.0, subforca = 0.0;
+	for(i = 0;i<m.numhabanj;i++){
+		for(j = 0;j<tam;j++){
+			if(Demonios[i].nome[j] == 'a'){
+			quantA++;
+			}
+		}
+		if(quantA == 2){
+		subforca = (Demonios[i].escuridao - (Demonios[i].visao/2));				   	
+        *c += (pow(subforca,2));
+		}
+		quantA = 0;
 	}
 }
 
-void printarhumanos (Humanos *humanos){
-	int i;
-    printf("\n%s %d\n", humanos[0].cidade,
-                        humanos[0].numhab);
-	for(i=1; i<=3;i++){
-	printf("\n%s %f\n",humanos[i].nome,
-                       humanos[i].forca);
-	}
+void maior(Anjos *anjo, Demonios *demonios, Humanos *humanos){
+     int i,j;
+     
+     //colocar os calculos de forca e comparar eles entre maior e menor
+		
 }
-*/
+
+void crescente( ){
+	//colocar as raças na ordem crescente a partir da soma das cidades de cada raça
+}
+
 int main(){
+  float calcA = 0.0;	
   Demonios *demonios = (Demonios*)calloc(TAM,sizeof(Demonios));
   Humanos *humanos = (Humanos*)calloc(TAM,sizeof(Humanos));
   Anjos *anjos = (Anjos*)calloc(TAM,sizeof(Anjos));
-  leranjos(anjos);
-  //lerdemonios(demonios);
-  lerhumanos(humanos);
+  Mundo mundo;
+  leranjos(anjos, &mundo);
+  lerhumanos(humanos, &mundo);
   lerdemonios(demonios);
-  
-  //printf("==================Anjos===================");
-   //printf("\n\n");
-  //printaranjos(anjos);
-  //printf("==================Humanos===================");
-   //printf("\n\n");
-  //printardemonios(demonios);
-  //printf("==================Demonios===================");
-   //printf("\n\n");
-  //printarhumanos(humanos);
+  somaAanjo(anjos,mundo,&calcA);
+  somaAhumano(humanos,mundo,&calcA);
+  somaAdemonios(demonios,mundo,&calcA);
+  printf("\nA forca dos individuos com duas letras a no nome e: %.2f\n", calcA);
   return(0);
 }
